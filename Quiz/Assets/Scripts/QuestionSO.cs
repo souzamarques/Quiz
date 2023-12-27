f8e291c9d34e67dd87e7664c4fef5ff64c487eea
Lucas Souza <lucassouzzamarques@gmail.com>
2023-12-26 21:54:51 -0300

    Just a getter method

--- Quiz/Assets/Scripts/QuestionSO.cs
+++ Quiz/Assets/Scripts/QuestionSO.cs
@@ -7,4 +7,9 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text";
+
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
