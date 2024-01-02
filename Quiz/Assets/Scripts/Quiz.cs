8465fc9656a9d66ff219079206a806bfe701f608
Lucas Souza <lucassouzzamarques@gmail.com>
2024-01-02 01:25:19 -0300

    Keeping score

--- Quiz/Assets/Scripts/Quiz.cs
+++ Quiz/Assets/Scripts/Quiz.cs
@@ -24,11 +24,15 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    ScoreKeeper scoreKeeper;
+    [SerializeField] TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         timer = FindObjectOfType<Timer>();
-        
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void Update()
@@ -64,6 +68,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -75,6 +80,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -94,6 +100,7 @@ public class Quiz : MonoBehaviour
             SetDefaultSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
     }
 
