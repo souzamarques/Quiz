b48bf572f47fdf37a59cf1c46c101ac722a4a86b
Lucas Souza <lucassouzzamarques@gmail.com>
2024-01-02 01:44:06 -0300

    Adding Slider to show game progress to player

--- Quiz/Assets/Scripts/Quiz.cs
+++ Quiz/Assets/Scripts/Quiz.cs
@@ -28,11 +28,18 @@ public class Quiz : MonoBehaviour
     ScoreKeeper scoreKeeper;
     [SerializeField] TextMeshProUGUI scoreText;
 
+    [Header("ProgressBar")]
+    [SerializeField] Slider progressBar;
+
+    public bool isComplete;
+
     // Start is called before the first frame update
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
     }
 
     void Update()
@@ -69,6 +76,11 @@ public class Quiz : MonoBehaviour
         SetButtonState(false);
         timer.CancelTimer();
         scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
+
+        if(progressBar.value == progressBar.maxValue)
+        {
+            isComplete = true;
+        }
     }
 
     void DisplayAnswer(int index)
@@ -100,6 +112,7 @@ public class Quiz : MonoBehaviour
             SetDefaultSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            progressBar.value++;
             scoreKeeper.IncrementQuestionsSeen();
         }
     }
